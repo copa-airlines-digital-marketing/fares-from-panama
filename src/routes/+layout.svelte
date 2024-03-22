c63247d54e7d5494c5d2018c365ac66fb77d3fca
César Patiño <cpatino@copaair.com>
2024-03-22 16:40:59 -0500

    added favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
+	import { CMS_IMAGE_URL } from '$lib/public/constants';
 	import '../app.css';
 </script>
+
+<svelte:head>
+	<link rel="shortcut icon" href="{CMS_IMAGE_URL}a0c674b2-eff8-4db3-b358-57355b9f9c5e" />
+</svelte:head>
 
 <main>
 	<slot />
