f20ea9a34b7619f2a95cd8e6cac9921b43a961b4
cpatinoCopaair <cesar.arturo.patino@gmail.com>
2024-04-09 08:02:23 -0500

    tabs start

--- src/lib/components/site/carousel/carousel-tabs.svelte
+++ src/lib/components/site/carousel/carousel-tabs.svelte
@@ -4,19 +4,25 @@
 	export let slides: Directus.CarrouselSlide[];
 </script>
 
-<Tabs.Root>
-	<Tabs.List>
+<Tabs.Root class="grid grid-cols-[72px_1fr_4fr_72px] grid-rows-[auto_1fr_auto]">
+	<Tabs.List class="col-start-2 flex flex-col row-start-2">
 		{#each slides as { title }}
-			<Tabs.Trigger value={title}>
+			<Tabs.Trigger class="font-heading p-16 text-h3 lg:text-h3 text-left" value={title}>
 				{title}
 			</Tabs.Trigger>
 		{/each}
 	</Tabs.List>
 	{#each slides as { title, description, imageID }}
-		<Tabs.Content value={title}>
-			<div>
+		<Tabs.Content
+			class="col-start-1 col-full grid-cols-subgrid grid-rows-subgrid [&:not([hidden])]:grid row-start-1 row-span-full"
+			value={title}
+		>
+			<div class="col-start-1 h-huge row-start-1"></div>
+			<div class="col-start-1 col-full row-start-1 row-span-full"></div>
+			<div class="col-start-3 max-w-prose p-24 row-start-2">
 				{@html description}
 			</div>
+			<div class="col-start-1 h-huge row-start-3"></div>
 		</Tabs.Content>
 	{/each}
 </Tabs.Root>
