55b9076d18bdf34f1bb4a981cbd2266dc9e22572
Cesar Patino <cpatino@copaair.com>
2024-05-08 14:37:43 -0500

    cleaned unused files

--- src/lib/components/site/skeleton-line.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let height = '16px';
-	export let width = '100%';
-	export let pulse = false;
-</script>
-
-<div class="flex items-center" style="height: {height};width: {width};">
-	<div class="bg-common-white h-12 rounded-full w-full" class:animate-pulse={pulse}></div>
-</div>
