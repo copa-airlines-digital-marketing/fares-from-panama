fa24b6b41ddeda170f71c5598675d10fa898abb0
cpatinoCopaair <cesar.arturo.patino@gmail.com>
2024-06-14 11:38:50 -0500

    adding card aspect ratios on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  darkMode: ['selector', '[data-mode="dark"]'],
+	darkMode: ['selector', '[data-mode="dark"]'],
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	presets: [require('cmds-tailwind-styles')],
 	theme: {
 		extend: {
 			screens: {
 				calendar: '1180px',
-        histogram: '1256px'
+				histogram: '1256px'
 			},
 			colors: {
 				red: '#e4002b'
@@ -18,6 +18,12 @@ export default {
 				'span-2': 'span 2 / span 2',
 				'span-3': 'span 3 / span 3',
 				'span-4': 'span 4 / span 4'
+			},
+			aspectRatio: {
+				'2/1': '2 / 1',
+				'4/3': '4 / 3',
+				'4/1': '4 / 1',
+				'3/4': '3 / 4'
 			}
 		}
 	}
