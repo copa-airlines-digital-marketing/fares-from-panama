40072a32fcd659355f726e2e568805bed1e952dc
Cesar Patino <cpatino@copaair.com>
2024-07-23 09:56:36 -0500

    Beta and realeased

---
+++ src/lib/components/pending/pending.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Heading from '../copa/typography/heading.svelte';
+	import Icon from '../site/icon.svelte';
+	import tarifasFelicesLogo from '$lib/assets/tarifas-viaja-panama-logo-white.svg?raw';
+	import copaIsotipo from '$lib/assets/logo-copa-airlines-isotipo.svg?raw';
+	import copaLogo from '$lib/assets/logo-copa-airlines.svg?raw';
+</script>
+
+<div class="fixed inset-0 bg-primary grid place-items-center">
+	<div>
+		<div class="flex gap-16 mb-24 items-center">
+			<Icon data={tarifasFelicesLogo} class="h-56 lg:h-72" />
+			<Icon data={copaIsotipo} class="h-40 sm:hidden" />
+			<Icon data={copaLogo} class="h-40 hidden sm:block" />
+		</div>
+		<Heading type="display-normal" style="text-common-white text-center">¡Esperalo pronto!</Heading>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,8 @@
 	import { setDaysContext } from '$lib/components/days';
 	import { setFareModulesContext } from '$lib/components/fare-modules/context.js';
 	import { Survey } from '$lib/components/survey';
+	import { PUBLIC_STATUS } from '$env/static/public';
+	import Pending from '$lib/components/pending/pending.svelte';
 
 	export let data;
 
@@ -54,9 +56,13 @@
 	<meta name="description" content={description} />
 </svelte:head>
 
-{#each storefrontSection as sectioM2M}
-	{@const section = sectioM2M.sections_id}
-	<Section {section}></Section>
-{/each}
+{#if PUBLIC_STATUS === 'Released'}
+	{#each storefrontSection as sectioM2M}
+		{@const section = sectioM2M.sections_id}
+		<Section {section}></Section>
+	{/each}
 
-<Survey></Survey>
+	<Survey></Survey>
+{:else}
+	<Pending />
+{/if}
